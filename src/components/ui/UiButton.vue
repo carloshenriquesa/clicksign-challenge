1904447e064429abe07bf7e318db6721fc6e0079
Carlos Henrique <carlos.hdsm@gmail.com>
2025-05-17 15:36:47 -0300

    init

---
+++ src/components/ui/UiButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button class="button">
+    <span>{{ icon }}</span>
+    {{ label }}
+  </button>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  label: string
+  icon?: string
+}
+import { defineProps } from 'vue'
+
+const { icon, label } = defineProps<Props>()
+</script>
